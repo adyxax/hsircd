8f6850bd4e066556c828c70269a0bfcf64eadb74
Julien Dessaux <judessaux@gmail.com>
2011-11-06 13:13:57 +0100

    Updated to support the latest crypto libs

--- Ircd/Utils.hs
+++ Ircd/Utils.hs
@@ -11,7 +11,6 @@ module Ircd.Utils
 
 import Control.Concurrent
 import Control.Monad.Reader
-import qualified Crypto.Cipher.RSA as RSA
 import qualified Data.ByteString as B
 import qualified Data.ByteString.Lazy as L
 import qualified Data.ByteString.Lazy.UTF8 as L8
@@ -19,7 +18,6 @@ import qualified Data.Certificate.KeyRSA as KeyRSA
 import Data.Certificate.PEM
 import Data.Certificate.X509
 import Data.List
-import Data.Maybe (fromMaybe)
 import Network
 import Network.Socket
 import Network.TLS
@@ -81,9 +79,11 @@ initTLSEnv tls = do
 readCertificate :: FilePath -> IO X509
 readCertificate filepath = do
     content <- B.readFile filepath
-    let certdata = fromMaybe (error "no valid certificate section") (parsePEMCert content)
-    let cert = case decodeCertificate $ L.fromChunks [certdata] of
-            Left err -> error $ "cannot decode certificate: " ++ err
+    let certdata = case parsePEMCert content of
+            Nothing -> error ("no valid certificate section")
+            Just x  -> x
+        cert = case decodeCertificate $ L.fromChunks [certdata] of
+            Left err -> error ("cannot decode certificate: " ++ err)
             Right x  -> x
     return cert
 
@@ -91,19 +91,9 @@ readPrivateKey :: FilePath -> IO PrivateKey
 readPrivateKey filepath = do
     content <- B.readFile filepath
     let pkdata = case parsePEMKeyRSA content of
-            Nothing -> error "no valid RSA key section"
+            Nothing -> error ("no valid RSA key section")
             Just x  -> L.fromChunks [x]
-    let pk = case KeyRSA.decodePrivate pkdata of
-            Left err -> error ("cannot decode key: " ++ err)
-            Right x  -> PrivRSA RSA.PrivateKey
-                { RSA.private_sz   = fromIntegral $ KeyRSA.lenmodulus x
-                , RSA.private_n    = KeyRSA.modulus x
-                , RSA.private_d    = KeyRSA.private_exponant x
-                , RSA.private_p    = KeyRSA.p1 x
-                , RSA.private_q    = KeyRSA.p2 x
-                , RSA.private_dP   = KeyRSA.exp1 x
-                , RSA.private_dQ   = KeyRSA.exp2 x
-                , RSA.private_qinv = KeyRSA.coef x
-                }
-    return pk
+    case KeyRSA.decodePrivate pkdata of
+        Left err -> error ("cannot decode key: " ++ err)
+        Right x  -> return $ PrivRSA $ snd x
 
