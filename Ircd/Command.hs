b4211052c44bfae40f4413754d3cc21dab825a7a
Julien Dessaux <judessaux@gmail.com>
2011-11-20 01:44:04 +0100

    Implemented the NICK command.

--- Ircd/Command.hs
+++ Ircd/Command.hs
@@ -4,14 +4,19 @@ module Ircd.Command
 
 import Control.Concurrent.MVar
 import Control.Monad.Reader
+import qualified Data.List as L
+import qualified Data.Map as M
+import Data.Maybe
 import qualified Network.IRC as IRC
 import System.Log.Logger
+import Text.Parsec
 
 import Ircd.Types
 import Ircd.Utils
 
 processPeerCommand :: IRC.Message -> PEnv (Env IO) ()
 processPeerCommand msg = do
+    penv <- ask
     pstateMV <- asks peerState
     pstate <- liftIO (readMVar pstateMV)
     let status = peerStatus pstate
@@ -21,7 +26,45 @@ processPeerCommand msg = do
                             pass:_ -> liftIO $ modifyMVar_ pstateMV (\st -> return st { peerPass = Just pass })
                             [] -> replyStr "461" ["PASS", "Not enough parameters"])
                     else replyStr "462" ["PASS", "You may not reregister"]
-        "NICK" -> liftIO $ errorM "Ircd.Command" $ "Command not implemented : " ++ IRC.msg_command msg
+        "NICK" -> do
+            when (status == UNREGISTERED) $ liftIO $ modifyMVar_ pstateMV (\st -> return st { peerStatus = REGISTERING })
+            case IRC.msg_params msg of
+                nick:stuff -> case parse nickName "" nick of
+                    Right nick' -> do
+                        -- If we got a valid nickname, we parse for the optional hopcount
+                        hopcount <- if peerIsServer pstate
+                          then case stuff of
+                              hopC:_ -> case reads hopC :: [(Int, String)] of
+                                  (hop,_):_ -> return hop
+                                  _ -> do liftIO . errorM "Ircd.Peer" $ "Bogus server peer " ++ show (peerClientAddr penv) ++ " :couldn't parse hopcount from server nick message : " ++ show stuff ; return 0
+                              _ -> do liftIO . errorM "Ircd.Peer" $ "Bogus server peer " ++ show (peerClientAddr penv) ++ " :no hopcount provided" ; return 0
+                          else return 0
+                        let msg' = IRC.Message (IRC.msg_prefix msg) "NICK" [nick', show (hopcount + 1) ]
+                        -- Then we try to set the nickname in the server state
+                        success <- lift (asks envIrcdState) >>= liftIO . flip modifyMVar (\st -> case M.lookup nick' (ircdNicks st) of
+                            Just _ -> return (st, False)
+                            Nothing -> return (st { ircdNicks = M.insert nick' penv $ ircdNicks st }, True))
+                        -- Finally we advertise to clients and servers | WARNING : cannot test until JOIN is implemented
+                        if success
+                          then (do
+                              let chans = peerChans pstate
+                              stMVar <- lift (asks envIrcdState)
+                              peers <- liftIO $ withMVar stMVar
+                                  (\st -> let nicks = L.nub . concat $ mapMaybe (flip M.lookup $ ircdChans st) chans
+                                          in return $ mapMaybe (flip M.lookup $ ircdNicks st) nicks)
+                              let peers' = if peerIsServer pstate
+                                             then filter (/= penv) peers
+                                             else peers
+                              -- TODO : check this, maybe it's not ok to send msg with hopcount to non server peers
+                              liftIO $ mapM_ (`sendTo` msg') peers')
+                          else if peerIsServer pstate
+                            then (replyStr "436" ["NICK", nick' ++ " :Nickname collision KILL"]
+                                -- TODO send KILLs for both the old nickname and the new one
+                                )
+                            else replyStr "433" ["NICK", nick' ++ " :Nickname is already in use"]
+                        return ()
+                    Left _ -> replyStr "432" ["NICK", nick ++ " :Erroneus nickname"]
+                [] -> replyStr "431" ["NICK", "No nickname given"]
         "USER" -> liftIO $ errorM "Ircd.Command" $ "Command not implemented : " ++ IRC.msg_command msg
         "SERVER" -> liftIO $ errorM "Ircd.Command" $ "Command not implemented : " ++ IRC.msg_command msg
         "OPER" -> liftIO $ errorM "Ircd.Command" $ "Command not implemented : " ++ IRC.msg_command msg
@@ -61,6 +104,13 @@ processPeerCommand msg = do
         "USERHOST" -> liftIO $ errorM "Ircd.Command" $ "Command not implemented : " ++ IRC.msg_command msg
         "ISON" -> liftIO $ errorM "Ircd.Command" $ "Command not implemented : " ++ IRC.msg_command msg
         _ -> liftIO $ errorM "Ircd.Command" $ "Invalid command in IRC message : " ++ IRC.msg_command msg
+  where
+    nickName = do
+        n <- letter
+        ick <- try (count 8 nickElt) <|> option [] (many1 nickElt)
+        _ <- eof
+        return $ n : ick
+    nickElt = alphaNum <|> oneOf "-[]\\`^{}"
 
 replyStr :: IRC.Command -> [IRC.Parameter] -> PEnv (Env IO) ()
 replyStr cmd params = do
